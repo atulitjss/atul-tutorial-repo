ffa9943bdd06783195dac69810045e7f6c7af74e
atul-yadav <atul.yadav@traveleasy.com>
2016-11-29 01:06:35 +0530

    oauth2 sspring sample application from spring official website

---
+++ tutorial-oauth2/src/main/java/com/atul/tutorial/oauth/HomeController.java
@@ -0,0 +1,14 @@
+package com.atul.tutorial.oauth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
---
+++ tutorial-oauth2/src/main/java/com/atul/tutorial/oauth/UserController.java
@@ -0,0 +1,27 @@
+package com.atul.tutorial.oauth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atul.tutorial.oauth.data.User;
+import com.atul.tutorial.oauth.data.UserRepository;
+
+
+
+@RestController
+public class UserController {
+
+	private final UserRepository userRepository;
+
+	@Autowired
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@RequestMapping("/users")
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+}
