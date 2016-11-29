9a26a196815aa384ffc9ba9be3f27e8db79322ec
atul-yadav <atul.yadav@traveleasy.com>
2016-11-29 12:45:20 +0530

    sample tutorial with spring boot actuator

---
+++ tutorial-springbootactuator/src/main/java/com/atul/tutitorial/springbootactuator/TutorialSpringbootactuatorApplication.java
@@ -0,0 +1,12 @@
+package com.atul.tutitorial.springbootactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringbootactuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringbootactuatorApplication.class, args);
+	}
+}
